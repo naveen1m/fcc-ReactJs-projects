8ede9fb46e24884d959105fe88afb8d000df2931
naveen <stupiddint@gmail.com>
2023-02-19 22:29:03 +0530

    final commit

---
+++ 02_tours/src/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <main className='loading'>
+        <h1>Loading...</h1>
+        <h4>wait a sec...</h4>
+      </main>
+  )
+}
+
+export default Loading
