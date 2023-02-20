ef7d87a6f5b7e9cc6b130d063c8d4000144e9110
naveen <stupiddint@gmail.com>
2023-02-20 10:27:10 +0530

    added some logic

--- 03_reviews/src/App.js
+++ 03_reviews/src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
+import Reviews from './Reviews';
 
 function App() {
   return (
-    <div className="App">
-      
-      hello there...!
-    </div>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>Our reviews</h2>
+          <div className='underline'></div>
+          <Reviews />
+        </div>
+      </section>
+    </main>
   );
 }
 
